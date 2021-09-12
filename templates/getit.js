cfe83228243988fbe693033ec493b9ba3123eb1d
franbvc <franbvc@gmail.com>
2021-09-12 16:16:25 -0300

    funcao delete funcionando

---
+++ templates/getit.js
@@ -0,0 +1,56 @@
+// // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the buttons that opens the modal
+// var allBtns = document.getElementById("edit-btn");
+
+// // Get the <span> elements that closes the modals
+// var spans = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Faz textarea aumentar a altura automaticamente
+  // Fonte: https://www.geeksforgeeks.org/how-to-create-auto-resize-textarea-using-javascript-jquery/#:~:text=It%20can%20be%20achieved%20by,height%20of%20an%20element%20automatically.
+  let textareas = document.getElementsByClassName("autoresize");
+  for (let i = 0; i < textareas.length; i++) {
+    let textarea = textareas[i];
+    function autoResize() {
+      this.style.height = "auto";
+      this.style.height = this.scrollHeight + "px";
+    }
+
+    textarea.addEventListener("input", autoResize, false);
+  }
+
+  // Sorteia classes de cores aleatoriamente para os cards
+  let cards = document.getElementsByClassName("card");
+  for (let i = 0; i < cards.length; i++) {
+    let card = cards[i];
+    card.className += ` card-color-${getRandomInt(
+      1,
+      5
+    )} card-rotation-${getRandomInt(1, 11)}`;
+  }
+});
---
+++ views.py
@@ -0,0 +1,47 @@
+from os import error, replace
+from utils import load_data, load_template
+import urllib
+from database import Database, Note
+
+def index(request):
+    db = Database('banco_notas')
+
+    if request.startswith('POST'):
+        # print(request)
+        # print("\n fim do request \n")
+        request = request.replace('\r', '')  # Remove caracteres indesejados
+        # Cabeçalho e corpo estão sempre separados por duas quebras de linha
+        partes = request.split('\n\n')
+        corpo = partes[1]
+        if corpo.split("=")[0] == 'delete':
+            id_int = int(corpo.split("=")[1])
+            db.delete(id_int)
+
+        else:
+            params = {}
+        # Preencha o dicionário params com as informações do corpo da requisição
+        # O dicionário conterá dois valores, o título e a descrição.
+        # Posteriormente pode ser interessante criar uma função que recebe a
+        # requisição e devolve os parâmetros para desacoplar esta lógica.
+        # Dica: use o método split da string e a função unquote_plus
+
+        #titulo=Sorvete+de+banana 
+        #detalhes=Coloque+uma+banana+no+congelador+e+espere.+Pronto%21+1%2B1%3D
+            for chave_valor in corpo.split('&'):
+                if chave_valor.startswith("titulo"):
+                    params["titulo"] = urllib.parse.unquote_plus(chave_valor[chave_valor.find("=")+1:], encoding="utf-8", errors="replace")
+                if chave_valor.startswith("detalhes"):
+                    params["detalhes"] = urllib.parse.unquote_plus(chave_valor[chave_valor.find("=")+1:], encoding="utf-8", errors="replace")
+
+            db.add(Note(title=params["titulo"], content=params["detalhes"]))
+
+    # Cria uma lista de <li>'s para cada anotação
+    # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+    note_template = load_template('components/note.html')
+    notes_li = [
+        note_template.format(title=dados.title, details=dados.content, id=dados.id)
+        for dados in db.get_all()
+    ]
+    notes = '\n'.join(notes_li)
+
+    return load_template('index.html').format(notes=notes).encode()
