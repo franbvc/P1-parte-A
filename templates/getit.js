635e1a2f149d75695f388c9bd4d3bb0dba3f907c
franbvc <franbvc@gmail.com>
2021-09-12 21:12:47 -0300

    tentando conceito A

--- templates/getit.js
+++ templates/getit.js
@@ -24,6 +24,59 @@
 //   }
 // }
 
+// Get the button that opens the modal
+var modalBtns = document.querySelectorAll("button.modal-button");
+
+// All page modal
+var modal = document.querySelector('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+for( let i = 0; i < modalBtns.length; i ++){
+    modalBtns[i].addEventListener("click", function(){
+      modal.style.visibility = "visible";
+    } );
+}
+
+for( let i = 0; i < spans.length; i ++){
+  spans[i].addEventListener("click", function(){
+    modal.style.visibility = "hidden";
+  } );
+}
+
+// for( let i = 0; i < spans.length; i ++){
+//   spans[i].addEventListener("click", function(){
+//     modal.classList.remove('modal-active')
+//   } );
+// }
+
+// // When the user clicks the button, open the modal
+// for (var i = 0; i < btn.length; i++) {
+//  btn[i].onclick = function(e) {
+//     e.preventDefault();
+//     modal = document.querySelector(e.target.getAttribute("href"));
+//     modal.style.display = "block";
+//  }
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// for (var i = 0; i < spans.length; i++) {
+//  spans[i].onclick = function() {
+//     for (var index in modals) {
+//       if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+//     }
+//  }
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target.classList.contains('modal')) {
+//      for (var index in modals) {
+//       if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+//      }
+//     }
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
